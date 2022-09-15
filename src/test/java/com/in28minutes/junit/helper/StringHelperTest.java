551b52951cccb12bf6f6496a429d41f5215ba6c3
Victor Toledo <victorptoledo@outlook.com>
2022-09-14 21:49:37 -0300

    feat(teste): Criação dos teste do método areFirstAndLastTwoCharactersTheSame
    BREAKING CHANGE: Criação dos testes restantes da classe StringHelper
    
    Signed-off-by: Victor Toledo <victorptoledo@outlook.com>

--- src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -2,25 +2,34 @@ package com.in28minutes.junit.helper;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 public class StringHelperTest {
 
     StringHelper stringHelper = new StringHelper();
 
     @Test
-    public void truncateAInFirst2PositionsOneA() {
+    public void testTruncateAInFirst2PositionsOneA() {
         assertEquals("CD", stringHelper.truncateAInFirst2Positions("ACD"));
     }
 
     @Test
-    public void truncateAInFirst2PositionsDoubleA() {
+    public void testTruncateAInFirst2PositionsDoubleA() {
         assertEquals("CD", stringHelper.truncateAInFirst2Positions("AACD"));
     }
 
     @Test
-    public void truncateAInFirst2PositionsTripleA() {
+    public void testTruncateAInFirst2PositionsTripleA() {
         assertNotEquals(stringHelper.truncateAInFirst2Positions("AAACD"), "CD");
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSameTrue() {
+        assertTrue("AD", stringHelper.areFirstAndLastTwoCharactersTheSame("ADACAD"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSameFalse() {
+        assertFalse("AD", stringHelper.areFirstAndLastTwoCharactersTheSame("AADCAD"));
     }
 }
