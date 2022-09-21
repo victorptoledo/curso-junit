866a456344c63e3c1ed54ecdfcd963deccacce22
Victor Toledo <victorptoledo@outlook.com>
2022-09-21 17:07:33 -0300

    feat: Testando comparação de arrays com assertArrayEquals
    BREAKING CHANGE: Exemplo prático de como funcionam o assertArrayEquals
    
    Signed-off-by: Victor Toledo <victorptoledo@outlook.com>

---
+++ src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -0,0 +1,22 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
+
+public class ArraysCompareTest {
+
+    // Arrays.sort
+    @Test
+    public void testArraysSort_RandomArray() {
+        int[] numbers = {12, 3, 4, 1};
+        int[] expected = {1, 3, 4, 12};
+
+        Arrays.sort(numbers);
+        assertArrayEquals(expected, numbers);
+
+    }
+}
